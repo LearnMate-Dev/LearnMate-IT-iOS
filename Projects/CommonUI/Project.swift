83d5b76c41df7fa70f0c6ad2fe415b2979f57f5f
iiuoon <bibi.urssu@gmail.com>
2025-06-30 13:37:27 +0900

    feat: feature modularize

---
+++ Projects/CommonUI/Project.swift
@@ -0,0 +1,15 @@
+import ProjectDescription
+
+let project = Project(
+    name: "CommonUI",
+    targets: [
+        .target(
+            name: "CommonUI",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "io.tuist.CommonUI",
+            sources: ["Sources/**"],
+            dependencies: []
+        )
+    ]
+)
