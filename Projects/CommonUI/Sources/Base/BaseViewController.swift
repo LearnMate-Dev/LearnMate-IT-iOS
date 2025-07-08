2f026681e84dcf2674e669dff8eca6c50fa9b786
Bibi-urssu <bibi.urssu@gmail.com>
2025-07-08 15:33:36 +0900

    feat:  BaseViewController setting

--- Projects/CommonUI/Sources/Base/BaseViewController.swift
+++ Projects/CommonUI/Sources/Base/BaseViewController.swift
@@ -4,3 +4,47 @@
 //
 //  Created by 박지윤 on 7/1/25.
 //
+
+import UIKit
+import RxSwift
+
+public protocol BaseViewItemProtocol: AnyObject {
+    func setupViewProperty()
+    func setupHierarchy()
+    func setupLayout()
+}
+
+public protocol BaseViewControllerProtocol: AnyObject, BaseViewItemProtocol {
+    func setupDelegate()
+    func setupBind()
+}
+
+open class BaseViewController: UIViewController, BaseViewControllerProtocol {
+    public let disposeBag = DisposeBag()
+    
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable, message: "remove required init")
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setupViewProperty()
+        setupDelegate()
+        setupHierarchy()
+        setupLayout()
+        setupBind()
+    }
+
+    open func setupViewProperty() { }
+    open func setupDelegate() { }
+    open func setupHierarchy() { }
+    open func setupLayout() { }
+    open func setupBind() { }
+}
