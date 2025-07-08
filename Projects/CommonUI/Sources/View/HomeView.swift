6403f1208d147fbe955170d5ceff3ad1402a6045
Bibi-urssu <bibi.urssu@gmail.com>
2025-07-08 15:34:33 +0900

    feat: Home UI Demo

--- Projects/CommonUI/Sources/View/HomeView.swift
+++ Projects/CommonUI/Sources/View/HomeView.swift
@@ -4,3 +4,58 @@
 //
 //  Created by 박지윤 on 7/1/25.
 //
+
+import Domain
+import UIKit
+import SnapKit
+import Then
+
+open class HomeView: UIView {
+    var superView = UIView()
+    var courseLabel = UILabel()
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        initAttribute()
+        initUI()
+    }
+    
+    public func bind(course: CourseVO) {
+        courseLabel.text = "bind"
+    }
+
+    func initAttribute() {
+        self.backgroundColor = .yellow
+        superView.backgroundColor = .green
+        superView.layer.cornerRadius = 28
+        superView.layer.masksToBounds = false
+
+        courseLabel = courseLabel.then {
+            $0.text = "init"
+            $0.textColor = .black
+            $0.font = .systemFont(ofSize: 20, weight: .bold)
+        }
+    }
+
+    func initUI() {
+        self.addSubview(superView)
+
+        superView.snp.makeConstraints {
+            $0.width.height.equalTo(300)
+            $0.top.equalToSuperview().inset(100)
+            $0.centerX.equalToSuperview()
+        }
+
+        [courseLabel]
+            .forEach { superView.addSubview($0) }
+
+        courseLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(20)
+            $0.centerX.equalToSuperview()
+        }
+    }
+
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Projects/Home/Sources/View/HomeViewController.swift
+++ Projects/Home/Sources/View/HomeViewController.swift
@@ -5,22 +5,44 @@
 //  Created by 박지윤 on 7/1/25.
 //
 
+import CommonUI
 import UIKit
+import SnapKit
+import RxSwift
 
-public class HomeViewController: UIViewController {
+public class HomeViewController: BaseViewController {
     let viewModel: HomeViewModel
-    
+    let homeView = HomeView()
+
     public init(homeViewModel: HomeViewModel) {
         self.viewModel = homeViewModel
-        super.init(nibName: nil, bundle: nil)
+        super.init()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     public override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+    }
+
+    public override func setupViewProperty() {
+    }
+    
+    public override func setupHierarchy() {
+        [homeView].forEach { view.addSubview($0) }
+    }
+
+    public override func setupDelegate() {
+    }
+
+    public override func setupLayout() {
+        homeView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    public override func setupBind() {
     }
 }
