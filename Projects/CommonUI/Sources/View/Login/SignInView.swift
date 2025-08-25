fa21d6852ab2c19fddac00cc27472c19fa930add
Bibi-urssu <bibi.urssu@gmail.com>
2025-08-25 17:25:33 +0900

    feat: implement SignInView UI

---
+++ Projects/CommonUI/Sources/View/Login/SignInView.swift
@@ -0,0 +1,110 @@
+//
+//  SignInView.swift
+//  CommonUI
+//
+//  Created by 박지윤 on 8/24/25.
+//
+
+import Domain
+import UIKit
+import SnapKit
+import RxSwift
+import Then
+
+open class SignInView: UIView {
+    let logoView = UIImageView().then {
+        $0.image = CommonUIAssets.logo
+        $0.contentMode = .scaleAspectFit
+    }
+
+    var textFieldStackView = UIStackView()
+    var idTextField = LMTextField()
+    var passwordTextField = LMTextField()
+    var loginButton = LMButton(textColor: CommonUIAssets.LMBlack,
+                               bgColor: CommonUIAssets.LMOrange1)
+    var signUpButton = UIButton()
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        initAttribute()
+        initUI()
+    }
+
+    public func bind(course: CourseVO) {
+        
+    }
+
+    func initAttribute() {
+        self.backgroundColor = .clear
+
+        textFieldStackView = textFieldStackView.then {
+            $0.axis = .vertical
+            $0.spacing = 20
+            $0.distribution = .fillEqually
+        }
+
+        idTextField = idTextField.then {
+            $0.placeholder = "아이디를 입력하세요."
+        }
+
+        passwordTextField = passwordTextField.then {
+            $0.placeholder = "비밀번호를 입력하세요."
+            $0.isSecureTextEntry = true
+        }
+
+        loginButton = loginButton.then {
+            $0.setTitle("로그인", for: .normal)
+        }
+
+        let signUpButtonTitle = "회원가입"
+        let attributedString = NSAttributedString(
+            string: signUpButtonTitle,
+            attributes: [
+                .foregroundColor: CommonUIAssets.LMGray1,
+                .font: UIFont.systemFont(ofSize: 16, weight: .regular),
+                .underlineStyle: NSUnderlineStyle.single.rawValue
+            ]
+        )
+
+        signUpButton = signUpButton.then {
+            $0.setAttributedTitle(attributedString, for: .normal)
+            $0.backgroundColor = .clear
+        }
+    }
+
+    func initUI() {
+        [logoView, textFieldStackView, loginButton, signUpButton]
+            .forEach { addSubview($0) }
+
+        [idTextField, passwordTextField]
+            .forEach { textFieldStackView.addArrangedSubview($0) }
+
+        logoView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(160)
+        }
+
+        textFieldStackView.snp.makeConstraints {
+            $0.top.equalTo(logoView.snp.bottom).offset(100)
+            $0.centerX.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(130)
+        }
+
+        loginButton.snp.makeConstraints {
+            $0.top.equalTo(textFieldStackView.snp.bottom).offset(33)
+            $0.centerX.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
+
+        signUpButton.snp.makeConstraints {
+            $0.top.equalTo(loginButton.snp.bottom).offset(35)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(27)
+        }
+    }
+
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
