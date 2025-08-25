18ed65852bde9bb1a848028b22815db47b0e3dfc
Bibi-urssu <bibi.urssu@gmail.com>
2025-08-25 17:23:21 +0900

    feat: add LMColor (LMGray4)

--- Projects/CommonUI/Sources/Enum/CommonUIAssets.swift
+++ Projects/CommonUI/Sources/Enum/CommonUIAssets.swift
@@ -58,6 +58,7 @@ public enum CommonUIAssets {
     public static let LMBlack = color(named: "LMBlack")
     public static let LMGray1 = color(named: "LMGray01")
     public static let LMGray3 = color(named: "LMGray03")
+    public static let LMGray4 = color(named: "LMGray04")
     public static let LMGray5 = color(named: "LMGray05")
     public static let LMGray6 = color(named: "LMGray06")
     public static let LMBlue = color(named: "LMBlue")
