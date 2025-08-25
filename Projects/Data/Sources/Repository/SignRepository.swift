fdf6f2f46a083f18f8c2429838968c24d7511094
Bibi-urssu <bibi.urssu@gmail.com>
2025-08-26 03:02:42 +0900

    feat: implement SignRepository

---
+++ Projects/Data/Sources/Repository/SignRepository.swift
@@ -0,0 +1,49 @@
+//
+//  SignRepository.swift
+//  Data
+//
+//  Created by 박지윤 on 8/26/25.
+//
+
+import Domain
+import RxSwift
+import Alamofire
+
+public class DefaultSignRepository: SignRepository {
+    private let tokenRepository: TokenRepository
+    
+    public init(tokenRepository: TokenRepository) {
+        self.tokenRepository = tokenRepository
+    }
+
+    public func postEmail(email: String) -> Completable {
+        return request(endpoint: "/api/auth/email")
+    }
+    
+    public func postConfirm(email: String, code: String) -> Completable {
+        return request(endpoint: "/api/auth/email/confirm")
+    }
+
+    private func request(endpoint: String) -> Completable {
+        return Completable.create { completable in
+            let url = "\(NetworkConfiguration.baseUrl)\(endpoint)"
+            var headers: HTTPHeaders = [:]
+            let request = AF.request(url,
+                                     method: .get,
+                                     encoding: URLEncoding.queryString,
+                                     headers: headers)
+                .validate()
+                .response { response in
+                    if let error = response.error {
+                        print("❌ API 응답 실패")
+                        completable(.error(error))
+                    } else {
+                        print("✅ API 응답 성공")
+                        completable(.completed)
+                    }
+                }
+
+            return Disposables.create { request.cancel() }
+        }
+    }
+}
