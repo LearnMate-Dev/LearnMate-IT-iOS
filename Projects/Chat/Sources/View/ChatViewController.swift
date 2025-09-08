7e7482edf78b647ee0b851a89c9d348213879c1c
Bibi-urssu <bibi.urssu@gmail.com>
2025-09-09 01:25:16 +0900

    feat: ChatViewController view 연결

--- Projects/Chat/Sources/View/ChatViewController.swift
+++ Projects/Chat/Sources/View/ChatViewController.swift
@@ -5,4 +5,48 @@
 //  Created by 박지윤 on 7/1/25.
 //
 
+import UIKit
+import CommonUI
 
+public class ChatViewController: BaseViewController {
+    let viewModel: ChatViewModel
+    let chatView = ChatView()
+
+    public init(chatViewModel: ChatViewModel) {
+        self.viewModel = chatViewModel
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewProperty()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    public override func setupViewProperty() {
+        view.backgroundColor = CommonUIAssets.LMOrange4
+    }
+
+    public override func setupHierarchy() {
+        view.addSubview(chatView)
+    }
+
+    public override func setupDelegate() {
+    }
+
+    public override func setupLayout() {
+        chatView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
