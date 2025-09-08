b1340b9b9550726fb4502eb3b39d047952e20c68
Bibi-urssu <bibi.urssu@gmail.com>
2025-09-09 01:01:32 +0900

    feat: ChatAssembly 구현

--- Projects/LearnMate/Sources/DI/ChatAssembly.swift
+++ Projects/LearnMate/Sources/DI/ChatAssembly.swift
@@ -5,3 +5,26 @@
 //  Created by 박지윤 on 7/2/25.
 //
 
+import Swinject
+import Chat
+import Domain
+
+/// Assembly: Swinject의 DI 등록을 위한 프로토콜
+public struct ChatAssembly: Assembly {
+    /// assemble(container:): 어떤 객체를 어떻게 등록할지 정의
+    public func assemble(container: Container) {
+        /// HomeViewModel을 DI 컨테이너에 등록
+        container.register(ChatViewModel.self) { resolver in
+            let chatUseCase = resolver.resolve(ChatUseCase.self)!
+            let tokenUseCase = resolver.resolve(TokenUseCase.self)!
+            return ChatViewModel(chatUseCase: chatUseCase,
+                                 tokenUseCase: tokenUseCase)
+        }
+
+        /// ViewModel을 DI 통해 주입받아 Controller를 생성함
+        container.register(ChatViewController.self) { resolver in
+            let chatViewModel = resolver.resolve(ChatViewModel.self)!
+            return ChatViewController(chatViewModel: chatViewModel)
+        }
+    }
+}
